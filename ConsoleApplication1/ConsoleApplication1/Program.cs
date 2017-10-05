15643fb0ed0520db64f77895647c80c13a60ac1f
Donald Hughes Jr <don.hughesjr@gmail.com>
2017-10-05 14:32:11 -0400

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            String strHostName = string.Empty; //getting the Host Name.
+            strHostName = Dns.GetHostName();
+            Console.WriteLine("Local Machine's Host Name " + strHostName);
+            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName); //Using Host Name.
+            IPAddress[] addr = ipEntry.AddressList;
+
+            for (int i = 0; i < addr.Length; i++)
+            {
+                Console.WriteLine("IP Address {1} : ", addr[i].ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
